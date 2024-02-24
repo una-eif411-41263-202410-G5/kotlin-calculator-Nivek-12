ec3408ededb1425440eef735efc5722c949dea0a
Nivek-12 <kevin737.94@gmail.com>
2024-02-23 19:35:09 -0600

    Calculadora
    Se realiza la clase calculadora y sus respectivos test

--- src/main/kotlin/design/patterns/calculadora/Calculadora.kt
+++ src/main/kotlin/design/patterns/calculadora/Calculadora.kt
@@ -1,4 +1,17 @@
 package design.patterns.design.patterns.calculadora
 
 class Calculadora {
+
+    fun suma(A: Int,B: Int): Int {
+        return A+B
+    }
+    fun resta(A: Int,B: Int): Int {
+        return A-B
+    }
+    fun multiplicacion(A: Int,B: Int): Int {
+        return A*B
+    }
+    fun division(A: Int,B: Int): Int {
+        return A/B
+    }
 }
--- src/test/kotlin/design/patterns/calculadora/CalculadoraTest.kt
+++ src/test/kotlin/design/patterns/calculadora/CalculadoraTest.kt
@@ -1,4 +1,33 @@
 package design.patterns.calculadora
 
+import design.patterns.design.patterns.calculadora.Calculadora
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
 class CalculadoraTest {
+
+    private val calculadora = Calculadora()
+    @Test
+    fun `test suma`() {
+        val result = calculadora.suma(10,15)
+        assertEquals(25,result)
+    }
+
+    @Test
+    fun `test resta`() {
+        val result = calculadora.resta(15,10)
+        assertEquals(5,result)
+    }
+
+    @Test
+    fun `test multiplicacion`() {
+        val result = calculadora.multiplicacion(5,2)
+        assertEquals(10,result)
+    }
+
+    @Test
+    fun `test division`() {
+        val result = calculadora.division(10,2)
+        assertEquals(5,result)
+    }
 }
